4dc19be1d113e99c8c99e9c9d771934ad1cd73cb
Asher <aantrim246@cedarville.edu>
2024-09-13 14:05:53 -0400

    Initial Commit

---
+++ Lab04(Laptop)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab04(Laptop)
+//
+//  Created by Asher Antrim on 9/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab04(Laptop)/Lab04_Laptop_App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab04_Laptop_App.swift
+//  Lab04(Laptop)
+//
+//  Created by Asher Antrim on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab04_Laptop_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
