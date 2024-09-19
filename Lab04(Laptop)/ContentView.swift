49dbfbc432e33f8ea9a22480ec0e1102b687f097
Asher <aantrim246@cedarville.edu>
2024-09-19 15:07:29 -0400

    made it so that more than just one card can be selected at a time

--- Lab04(Laptop)/ContentView.swift
+++ Lab04(Laptop)/ContentView.swift
@@ -11,18 +11,18 @@ struct ContentView: View {
                     "A♠️", "2♠️", "3♠️", "4♠️", "5♠️", "6♠️", "7♠️", "8♠️", "9♠️", "10♠️", "J♠️", "Q♠️", "K♠️"]
     
     @State private var shuffledCards: [String] = []
-    @State private var selectedCard: String?
+    @State private var selectedCards: Set<String> = []
     
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns) {
                 ForEach(shuffledCards, id: \.self) { card in
-                    CardView(card: card, isSelected: selectedCard == card)
+                    CardView(card: card, isSelected: selectedCards.contains(card))
                         .onTapGesture {
-                            if selectedCard == card {
-                                selectedCard = nil
+                            if selectedCards.contains(card) {
+                                selectedCards.remove(card)
                             } else {
-                                selectedCard = card
+                                selectedCards.insert(card)
                             }
                         }
                 }
